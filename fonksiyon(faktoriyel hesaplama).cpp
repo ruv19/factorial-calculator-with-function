18e6a306f12f825a895de5597a03b41714439f47
Rüveyda Dal <ruveydadal.3@gmail.com>
2022-08-11 16:41:09 +0300

    Update fonksiyon(faktoriyel hesaplama).cpp

--- fonksiyon(faktoriyel hesaplama).cpp
+++ fonksiyon(faktoriyel hesaplama).cpp
@@ -1,19 +1,22 @@
 #include <stdio.h>
+
 void faktoriyel_al(int a);
-int main (void)
-{
+int main (void) {
+
 	int a;
 	printf("\n faktoiyelini almak istediginiz sayiyi giriniz:");
 	scanf("%d",&a);
 	faktoriyel_al(a);
 	return 0;
 }
-void faktoriyel_al(int a)
-{
+
+void faktoriyel_al(int a) {
+
 	int i,sonuc=1;
-	for(i=1;i<=a;i++)
-	{
-		sonuc=sonuc*i;
+	for(i=1; i<=a ;i++) {
+
+		sonuc = sonuc*i;
 	}
+    
 	printf("\n %d sayisinin faktoriyeli=%d",a,sonuc);
 }
